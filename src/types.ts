e1e4b1d88b81043c5e4f48135641e8487822da2a
foxmon <jjockrod@naver.com>
2023-01-11 21:41:51 +0900

    types

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+// Promise, or maybe not
+// PromiseLike<T> => then만 존재
+// Promise<T> => finally만 있음
+export type Awaitable<T> = T | PromiseLike<T>;
+
+// Null or whatever
+export type Nullable<T> = T | null | undefined;
+
+// Function
+export type Fn<T = void> = () => T;
