1a985eb799698605496cf66e632707486ad0b614
foxmon <jjockrod@naver.com>
2023-01-19 21:32:16 +0900

    array

---
+++ src/array.ts
@@ -0,0 +1,6 @@
+import { Arrayable, Nullable } from "./types";
+
+export const toArray = <T>(array?: Nullable<Arrayable<T>>): Array<T> => {
+  array = array ?? [];
+  return Array.isArray(array) ? array : [array];
+};
