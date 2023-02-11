341408b9815d6d0020d73279a5522d46b96c7050
foxmon <jjockrod@naver.com>
2023-02-11 14:18:59 +0900

    Array

--- src/array.ts
+++ src/array.ts
@@ -4,3 +4,8 @@ export const toArray = <T>(array?: Nullable<Arrayable<T>>): Array<T> => {
   array = array ?? [];
   return Array.isArray(array) ? array : [array];
 };
+
+export const mergeArray = <T>(arr1: Array<T>, arr2: Array<T>): Array<T> => {
+  const mergedArray: Array<T> = [...arr1, ...arr2];
+  return mergedArray;
+};
