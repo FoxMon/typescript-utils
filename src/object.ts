2f58149ed32c7dc500651f5f04ad081e3561c5c1
foxmon <jjockrod@naver.com>
2023-02-11 14:19:03 +0900

    Object

--- src/object.ts
+++ src/object.ts
@@ -1,7 +1,15 @@
-function isObject(target: unknown): boolean {
+export function isObject(target: unknown): boolean {
   return Object.prototype.toString.call(target) === "[object Object]";
 }
 
 export function isMergableObject(item: any): boolean {
   return isObject(item) && !Array.isArray(item);
+}
+
+export function mergeObject(o1: any, o2: any): any {
+  const newObj = Object.assign({}, o2);
+  for (const key in o1) {
+    newObj[key] = o1[key];
+  }
+  return newObj;
 }
