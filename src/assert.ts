734b8a33689abd7b7fef1484ba00b92e1987ef05
foxmon <jjockrod@naver.com>
2023-03-22 21:17:28 +0900

    assert

--- src/assert.ts
+++ src/assert.ts
@@ -1,3 +1,21 @@
+export class AssertionError {
+  constructor(msg: string) {
+    throw new Error(msg);
+  }
+}
+
 export function assert(condition: boolean, message: string): asserts condition {
   if (!condition) throw new Error(message);
+}
+
+export function assertIsString(value: unknown): asserts value is string {
+  if (typeof value !== "string") {
+    throw new AssertionError("Not a string!");
+  }
+}
+
+export function assertIsNumber(value: unknown): asserts value is number {
+  if (typeof value !== "number") {
+    throw new AssertionError("Not a number!");
+  }
 }
